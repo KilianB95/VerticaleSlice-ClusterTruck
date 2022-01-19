6e3f91c599b1bcedc406e1a2740f9b5d8e42e5bd
rickdohmen <70955143+rickdohmen@users.noreply.github.com>
2022-01-19 13:13:54 +0100

    jump werkt voor de helft

--- Assets/Scripts/MovementCharacterSripts/PlayerMovement.cs
+++ Assets/Scripts/MovementCharacterSripts/PlayerMovement.cs
@@ -6,24 +6,25 @@ public class PlayerMovement : MonoBehaviour
 {
     CharacterController characterController;
     public float MovementSpeed = 15;
-    public float Gravity = 9.8f;
-    private float velocity = 0;
-    public float Jumpforce = 3;
-    public bool isGrounded;
+    public float Gravity = 1.0f;
+    public float Jumpforce = 0.5f;
+    public Vector3 jump;
+     bool isGrounded = true;
     [SerializeField] private Rigidbody rb;
 
     private void Start()
     {
         characterController = GetComponent<CharacterController>();
+        rb = GetComponent<Rigidbody>();
+        jump = new Vector3(0.0f, 0.0f, 0.0f);
     }
-
+    // detecteert of je op de grond staat
     private void OnCollisionStay(Collision collision)
     {
-        if (collision.gameObject.tag == "Ground")
-        {
+        
             isGrounded = true;
-            Debug.Log("weck");
-        }
+            Debug.Log(isGrounded);
+        
     }
 
     private void Update()
@@ -31,16 +32,28 @@ public class PlayerMovement : MonoBehaviour
         // player movement - forward, backwards, left, right
         float horizontal = Input.GetAxis("Horizontal") * MovementSpeed;
         float vertical = Input.GetAxis("Vertical") * MovementSpeed;
-        characterController.Move((Vector3.right * horizontal + Vector3.forward * vertical) * Time.deltaTime);
+        characterController.Move((Vector3.right * horizontal + Vector3.forward * vertical + jump) * Time.deltaTime);
+
 
         //Gravity
 
-        if(Input.GetKeyDown(KeyCode.Space) && isGrounded)
+
+
+        // player jump - to jump up 
+        
+        if (Input.GetKey(KeyCode.Space))
         {
-            rb.AddForce(new Vector3(0, Jumpforce), ForceMode.Impulse);
-            Debug.Log("spatie werkt");
+            
+            rb.AddForce(jump * Jumpforce, ForceMode.Impulse);
+            jump = new Vector3(0.0f, 1.0f, 0.0f);
+            isGrounded = false;
+            
+
+            Debug.Log(KeyCode.Space);
+            
            
         }
+        
        
     }
 
